e6845f4fd169a77d857a7e0203dc248e94ff2230
jerome-labonte-udem <jerome.labonte@umontreal.ca>
2020-02-14 16:46:03 -0500

    Add pickle generation

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 import pandas as pd
+from pathlib import Path
 from typing import List, Dict, Any, AnyStr, Tuple, Optional
 import datetime
+import pickle
+import random
+
 from src.schema import Station
 from src.utils.data_utils import (
     get_labels_list_datetime, get_hdf5_samples_from_day, random_timestamps_from_day, get_metadata,
@@ -107,6 +111,69 @@ def hdf5_dataloader_list_of_days(
     timesteps = len(previous_time_offsets)
     target_len = len(target_time_offsets)
     # TODO: Check if that's how prefetch should be used
+    data_loader = tf.data.Dataset.from_generator(
+        data_generator, output_types=((tf.float32, tf.float32, tf.float32), tf.float32),
+        output_shapes=(((None, timesteps, patch_size[0], patch_size[1], 5),
+                        (None, timesteps, past_metadata_len),
+                        (None, future_metadata_len)),
+                       (None, target_len))
+    ).prefetch(tf.data.experimental.AUTOTUNE)
+
+    return data_loader
+
+
+def hdf5_dataloader_pickles(pickle_path,
+                            subset,
+                            target_time_offsets: List[datetime.timedelta],
+                            previous_time_offsets: List[datetime.timedelta],
+                            patch_size: Tuple[int, int] = (32, 32),
+                            ) -> tf.data.Dataset:
+    """
+    * Train time *: Dataloader that takes as argument a list of days
+    (datetime.datetime but where the only thing that matters is the path to the hdf5_file).
+    We then sample "batch_size" timestamps from that day, and get a total of
+    num_stations * batch_size samples for that day
+    * Test time *: Take as input a list of target_datetimes (same as required by script in evaluator.py)
+    Args:
+        dataframe: a pandas dataframe that provides the netCDF file path (or HDF5 file path and offset) for all
+            relevant timestamp values over the test period.
+        target_datetimes:
+        target_time_offsets: the list of timedeltas to predict GHIs for (by definition: [T=0, T+1h, T+3h, T+6h]).
+        config: configuration dictionary holding any extra parameters that might be required by the user. These
+            parameters are loaded automatically if the user provided a JSON file in their submission. Submitting
+            such a JSON file is completely optional, and this argument can be ignored if not needed.
+        batch_size: Samples per batch. -- The real batch_size will be num_stations * batch_size --
+        data_directory: (Optional) Provide a data_directory if the directory is not the same as
+        the paths from the dataframe
+        subset: "test", "valid", or "train
+        patch_size:
+        stations:
+        previous_time_offsets: list of timedelta of previous pictures that we want to look at,
+        if not provided we only look at t0
+    Returns:
+        A ``tf.data.Dataset`` object that can be used to produce input tensors for your model. One tensor
+        must correspond to one sequence of past imagery data. The tensors must be generated in the order given
+        by ``target_sequences``.
+    """
+    if subset not in ["train", "val"]:
+        raise ValueError(f"Invalid subset string provided = {subset}")
+
+    pathlist = list(Path(pickle_path).glob(f'{subset}_*.pkl'))
+    print(pathlist)
+    if subset == "train":
+        random.shuffle(pathlist)
+    print("pathlist after shuffle", pathlist)
+
+    def data_generator():
+        for path in pathlist:
+            (samples, past_metadata, future_metadata), targets = pickle.load(open(path, "rb"))
+            yield (samples, past_metadata, future_metadata), targets
+
+    past_metadata_len = 5  # day, hour, min, daytime, Clearsky
+    future_metadata_len = len(target_time_offsets)
+    timesteps = len(previous_time_offsets)
+    target_len = len(target_time_offsets)
+
     data_loader = tf.data.Dataset.from_generator(
         data_generator, output_types=((tf.float32, tf.float32, tf.float32), tf.float32),
         output_shapes=(((None, timesteps, patch_size[0], patch_size[1], 5),
