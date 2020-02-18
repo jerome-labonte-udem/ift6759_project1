779669cecff8f9969bf7e09fdce188c706b96098
Phil Beardsell <philbeardsell@gmail.com>
2020-02-18 09:47:23 -0500

    forgot to modify example of bash script to run

--- test_run_model_tfrecord.sh
+++ test_run_model_tfrecord.sh
@@ -6,7 +6,6 @@
 
 FOLDER="/project/cq-training-1/project1/teams/team09"
 ZIP_FILE="tf_records.zip"
-SAVE_PATH="${FOLDER}/saved_models/vgg2d"
 
 # 1. Create your environement locally
 source "${FOLDER}/venv/bin/activate"
@@ -21,7 +20,7 @@ unzip "${SLURM_TMPDIR}/${ZIP_FILE}" -d "${SLURM_TMPDIR}"
 # 4. Launch your job, tell it to save the model in $SLURM_TMPDIR
 #    and look for the dataset into $SLURM_TMPDIR
 python ${FOLDER}/ift6759_project1/train.py \
-        --save_dir "${SAVE_PATH}" \
+        --save_dir "${FOLDER}" \
         --cfg_path "${FOLDER}/ift6759_project1/config_files/train_config_philippe.json" \
         --data_path "${SLURM_TMPDIR}/${FOLDER}/tf_records"
 
